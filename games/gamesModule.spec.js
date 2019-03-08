c10ef72a76320e81bc29197ae13028b2cfe7e68f
Jacob Bryan <jtb36688@gmail.com>
2019-03-08 13:07:42 -0500

    Added test for get ORM helper which checks GET length against table length

--- games/gamesModule.spec.js
+++ games/gamesModule.spec.js
@@ -2,23 +2,30 @@
 const db = require('../data/dbConfig.js')
 const games = require('./gamesModule.js')
 
-describe('gamesModule', () => {
+describe('gamesModule ORM for "games" table', () => {
     describe('add()', () => {
         afterEach(async () => {
             await db.seed.run()
         })
-        it('should add the provided game into the db', async () => {
+        it('should add the provided game into "games" table and return data from the record of that game', async () => {
             let game = await games.add({name: 'Super Mario Bros.', genre: "Platformer"})
             expect(game.genre).toBe('Platformer')
             let game2 = await games.add({name: 'Minecraft', genre: 'Adventure'})
             expect(game2.name).toBe('Minecraft')
         })
-        it('should add the provided amount of games into the db', async () => {
+        it('should add be able to consecutively insert records into "games" table', async () => {
             await games.add({name: 'Super Mario RPG: Legend of the Seven Stars', genre: 'RPG'})
             await games.add({name: 'Minecraft', genre: 'Adventure'})
             await games.add({name: 'Enter the Gungeon', genre: 'Shooter'})
-            const gamesdb = await db('games')
+            let gamesdb = await db('games')
             expect(gamesdb).toHaveLength(6)
         })
+    })
+    describe('get()', () => {
+        it('should return an array that has the length equal to amount of records in "games" table', async () => {
+            let getgames = await games.get()
+            let gamesdb = await db('games')
+            expect(getgames.length()).toEqual(gamesdb.length())
+        }
     })
 })
